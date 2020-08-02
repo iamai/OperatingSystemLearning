026a260ccc8275fdf870cb74d68ddaef3017409a
iamai <13146748016@163.com>
2020-08-02 10:24:19 +0800

    fix

--- homework/processApi2.c
+++ homework/processApi2.c
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[]) {
     FILE *fp;
     char buff[255];
-    fp = fopen("testFile", "w+");
+    fp = fopen("testFile", "r");
 
     int p = fork();
     if (p < 0) {
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
     } else if (p == 0) {
         printf("I am the child %d\n", getpid());
 
-        fscanf(fp, "%s", buff);
+        fgets(buff, 10, fp);
         printf("%s\n", buff);
         fclose(fp);
 
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]) {
     } else {
         printf("I am the parert of %d\n", p);
 
-        fscanf(fp, "%s", buff);
+        fgets(buff, 10, fp);
         printf("%s\n", buff);
         fclose(fp);
 
