db05f860d8e6ee446751691611d479f1c729752e
iamai <13146748016@163.com>
2020-08-02 10:48:19 +0800

    add processApi4.c

---
+++ homework/processApi4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+
+    int p = fork();
+    if (p < 0) {
+        fprintf(stderr, "some error happen");
+        exit(1);
+    } else if (p == 0) {
+        printf("I am the child %d\n", getpid());
+
+        char *args[] = {"./test", 1, "23"};
+        execvp("./test", args);
+
+        printf("child end\n");
+    } else {
+        printf("I am the parert of %d\n", p);
+        printf("parent end\n");
+    }
+
+    return 0;
+}
